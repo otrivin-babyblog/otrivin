81bb4f41a66d2ccd251d49ca837dbe22cd5dba59
otrivin-babyblog <otrivin.babyblog@yandex.ru>
2016-10-28 09:37:28 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -38,4 +38,13 @@ $source = 'http://promo.babyblog.ru/otrivin_baby/';
 	  <script src="<? echo $source; ?>js/jquery.min.js"></script>
 
     <script src="https://st.babyblog.ru/st20330/js/api/build/babyblog/bbapi.min.js"></script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-86413935-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
 </head>
